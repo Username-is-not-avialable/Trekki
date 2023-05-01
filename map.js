a360f865374f39ec109379dd207d034b2851d947
username-is-not-avialable <antonbakir280@mail.ru>
2023-05-01 14:38:59 +0500

    add simple clickable route

--- map.js
+++ map.js
@@ -4,3 +4,10 @@ const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+let gpx = "Mitryasov_Kamchatka_3pesh_2022.gpx";
+new L.GPX(gpx, { async: true })
+  .on("loaded", function (e) {
+    map.fitBounds(e.target.getBounds());
+  })
+  .on("click", (e) => alert("Clicked on route"))
+  .addTo(map);
