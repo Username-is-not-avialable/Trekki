f211947dbbd2b1add72032dfccb350c46466dfda
username-is-not-avialable <antonbakir280@mail.ru>
2023-05-17 14:17:17 +0500

    initialize backend folder move server files to backend folder

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+// TODO: app.static(); // для отдачи статического контента
+
+let JSONData;
+let r = new Promise(function (resolve, reject) {
+  fs.readFile("table.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    JSONData = Parse(data);
+  });
+  resolve(JSONData);
+});
+r.then(
+  (result) => {
+    JSONData = result;
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+function Parse(str) {
+  trackReportPairs = str.split(",");
+  let route = {};
+  for (var TRPair of trackReportPairs) {
+    let [trek, report] = TRPair.split(" ");
+    route[trek] = report;
+  }
+  return JSON.stringify(route);
+}
+
+app.get("/getAllTracks", (req, res) => {
+  res.send(JSONData);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
