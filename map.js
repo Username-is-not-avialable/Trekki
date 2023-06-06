6218fd6b563f588ce1dd82dde3af3b7c98197a0a
username-is-not-avialable <antonbakir280@mail.ru>
2023-06-07 01:09:36 +0500

    add layer control with OSM, OpenTopoMap

--- map.js
+++ map.js
@@ -1,19 +1,39 @@
 const rootURL = "http://localhost:3000/";
+const OSMLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+});
+const OpenTopoMapLayer = L.tileLayer(
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 17,
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+  }
+);
 
 main();
 async function main() {
   map = await setMap();
+  AddLayersControl(map);
   routesObject = await getRoutesObject();
   addTracks(routesObject, map);
 }
 
+//добавление переключателя слоев
+function AddLayersControl(map) {
+  let baseLayers = {
+    OpenStreetMap: OSMLayer,
+    OpenTopoMap: OpenTopoMapLayer,
+    // Yandex: YandexMap,
+  };
+  L.control.layers(baseLayers).addTo(map);
+}
+
 async function setMap() {
   const map = L.map("map").setView([56.840449, 60.657217], 9);
-  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
+  OSMLayer.addTo(map);
   return map;
 }
 
